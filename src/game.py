ef2124c63dadef4c4676b9d8e005ddc2f4cf3553
Michael Maquera <maquera.m@northeastern.edu>
2022-03-24 20:13:44 -0400

    adding more docs

--- src/game.py
+++ src/game.py
@@ -7,7 +7,19 @@ class Game:
         self.screen = pygame.display.set_mode((window_width, window_height))
         self.clock = pygame.time.Clock()
     
+    def create(self):
+        print("how many characters? ")
+    # get user input for # of characters
+    
+    # create the map
+    #createMap(); 
+    
+    # create the characters 
+    # createChars();
+    
+    # primary game loop
     def run(self):
+        
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -15,6 +27,7 @@ class Game:
                     sys.exit()
 
             self.screen.fill('black')
+            
             pygame.display.update()
             self.clock.tick(FPS)
 
--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,3 @@
 window_height = 800;
 window_width = 1400;
-
+FPS = 30;
